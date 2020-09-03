0706a230ff84f93cdfd7e5da5daf437e50ad1c65
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-09-03 14:49:42 -0700

    added a badge, and ToC, ascreenshot and a demo, fixed some spelling errors

--- generatemarkdown.js
+++ generatemarkdown.js
@@ -1,22 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} 
-  # Description
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [license](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+# Description
   ${data.description} 
-  # Installation
+# Installation
   ${data.installation}
-  # Usage
+# Usage
   ${data.usage}
-  # Tests
+# Tests
   ${data.tests}
-  # license
-  ${data.license}
-  # Contributimg
+# license
+  #### ${data.license}
+  [![License: WTFPL](https://img.shields.io/badge/License-WTFPL-darkgreen.svg)](http://www.wtfpl.net/about/)
+# Contributing
   ${data.contributing}
-  # Questions
-  you can find my github profile here: ${data.username}. 
-  If you haveany questions about my projects you can contact me: ${data.email}
-    `;
+# Questions
+  #### you can find my github profile here: ${data.username}
+  #### If you haveany questions about my projects you can contact me: ${data.email}
+  `;
   }
   
   module.exports = generateMarkdown;
